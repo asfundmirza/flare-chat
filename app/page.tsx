fe449237d7bb02c14e0612822fd260d51f85278c
asfundmirza <asfundmirzaj@gmail.com>
2023-12-02 23:36:31 +0500

    starting navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
+import { currentUser } from "@clerk/nextjs";
 import { UserButton } from "@clerk/nextjs";
 import { User } from "@clerk/nextjs/server";
-export default function Home() {
-  return <UserButton />;
+
+export default async function Home() {
+  const user: userInfo | null = await currentUser();
+
+  return (
+    <div>
+      <UserButton />
+      {user && user.id};
+    </div>
+  );
 }
---
+++ typings.d.ts
@@ -0,0 +1,6 @@
+interface userInfo {
+  id;
+  imageUrl;
+  firstName;
+  lastName;
+}
