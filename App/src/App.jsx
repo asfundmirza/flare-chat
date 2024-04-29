55b01e448508a373cbda1f9863907d3caba4edfb
asfundmirza <asfundmirzaj@gmail.com>
2024-04-29 17:59:16 +0500

    added the background and backdrop blur

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-// import "./App.css";
+
 const App = () => {
-  return <div className=" ">App</div>;
+  return (
+    <div class="flex items-center justify-center h-screen">
+      <div class="container mx-auto h-[80vh] bg-black/50  rounded-lg text-white backdrop-blur-sm border border-gray-400/10 ">
+        <div>
+          <p>APP</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
