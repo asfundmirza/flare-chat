0b87e0f116863b00bf2c17cf90576b3c48bcbee6
asfundmirza <asfundmirzaj@gmail.com>
2024-05-01 18:42:32 +0500

    added receivers details section

--- App/src/components/receiverDetail/BottomSection/BottomSection.jsx
+++ App/src/components/receiverDetail/BottomSection/BottomSection.jsx
@@ -1,9 +1,66 @@
 import React from "react";
 import ArrowUp from "../../../assets/icons/arrowUp.png";
 import ArrowDown from "../../../assets/icons/arrowDown.png";
+import DloadImg from "../../../assets/icons/bg.jpg";
+import Downloadicon from "../../../assets/icons/download.png";
 
 const BottomSection = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col mt-4  h-full justify-between">
+      <div className="flex flex-col gap-3 ">
+        <div className="flex justify-between">
+          <div>
+            <p>Chat Settings</p>
+          </div>
+          <div className=" bg-slate-400/10 p-2 rounded-full">
+            <img src={ArrowUp} className="w-[10px] h-[10px]" />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <p>Chat Settings</p>
+          </div>
+          <div className=" bg-slate-400/10 p-2 rounded-full">
+            <img src={ArrowUp} className="w-[10px] h-[10px]" />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <p>Privacy & Policy</p>
+          </div>
+          <div className=" bg-slate-400/10 p-2 rounded-full">
+            <img src={ArrowUp} className="w-[10px] h-[10px]" />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <p>Shared Files</p>
+          </div>
+          <div className=" bg-slate-400/10 p-2 rounded-full">
+            <img src={ArrowDown} className="w-[10px] h-[10px]" />
+          </div>
+        </div>
+
+        <div className="flex justify-between px-5 mt-5">
+          <div className="flex gap-4">
+            <img src={DloadImg} alt="dloadImg" className="w-[20px] h-[20px]" />
+            <span className=" text-gray-400 text-xs">photo.png</span>
+          </div>
+          <div>
+            <img
+              src={Downloadicon}
+              alt="download"
+              className="w-[15px] h-[15px]"
+            />
+          </div>
+        </div>
+      </div>
+
+      <button className=" justify-center w-full p-2 bg-red-900/50 hover:bg-red-500 text-center">
+        Block
+      </button>
+    </div>
+  );
 };
 
 export default BottomSection;
--- App/src/components/receiverDetail/ReceiverDetail.jsx
+++ App/src/components/receiverDetail/ReceiverDetail.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import TopSection from "./TopSection/TopSection";
+import BottomSection from "./BottomSection/BottomSection";
 const ReceiverDetail = () => {
   return (
-    <div className="flex-1 p-5 flex flex-col">
+    <div className="flex-1 p-5 flex flex-col h-full">
       <TopSection />
+      <BottomSection />
     </div>
   );
 };
