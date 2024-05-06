0d1f290dc7b5bf3b8ac0b62cb8ebdf5b2ac82612
asfundmirza <asfundmirzaj@gmail.com>
2024-05-06 11:29:04 +0500

    added sign out button

--- App/src/components/dashboard/Dashboard.jsx
+++ App/src/components/dashboard/Dashboard.jsx
@@ -14,8 +14,8 @@ const Dashboard = () => {
         backgroundPosition: "center",
       }}
     >
-      <div className="flex items-center w-full bg-black/30 backdrop-blur-sm justify-center h-screen">
-        <div className="container mx-auto flex   h-[80vh]  bg-black/10  rounded-lg text-white backdrop-blur-sm border border-gray-400/10 ">
+      <div className="flex items-center  bg-black/30 backdrop-blur-sm justify-center h-screen">
+        <div className="container p-0 mx-auto flex h-[80vh]  bg-black/10  rounded-lg text-white backdrop-blur-sm border border-gray-400/10 ">
           <Lists />
           <Chats />
           <ReceiverDetail />
--- App/src/components/lists/userDetail/UserDetail.jsx
+++ App/src/components/lists/userDetail/UserDetail.jsx
@@ -3,6 +3,15 @@ import Avatar from "../../../assets/icons/avatar.png";
 import More from "../../../assets/icons/more.png";
 import Video from "../../../assets/icons/video.png";
 import Edit from "../../../assets/icons/edit.png";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../../components/ui/dropdown-menu";
+import { Button } from "../../../components/ui/button";
 
 const UserDetail = () => {
   return (
@@ -17,16 +26,26 @@ const UserDetail = () => {
       </div>
 
       <div className="flex gap-3 ">
-        <img
-          src={More}
-          alt="more"
-          className="w-[20px] h-[20px] cursor-pointer"
-        />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <img
+              src={More}
+              alt="edit"
+              className="w-[20px] h-[20px] cursor-pointer"
+            />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="flex justify-center bg-black text-white outline-none border-none">
+              Sign out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <img
           src={Video}
           alt="video"
           className="w-[20px] h-[20px] cursor-pointer"
         />
+
         <img
           src={Edit}
           alt="edit"
